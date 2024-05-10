aa9ab521716f47667a8dba45a6722299027578b8
aradhyabagrodia <aradhyabagr@gmail.com>
2024-05-10 17:12:10 +0530

    saveproject

---
+++ app/login/page.jsx
@@ -0,0 +1,16 @@
+import styles from "@/app/ui/login/login.module.css"
+const LoginPage = () => {
+    return (
+        <div className={styles.container}>
+            <form action="" className={styles.form}>
+            <h1>Log In</h1>
+
+                <input type="text" placeholder="username" />
+                <input type="password" placeholder="password" />
+                <button>Log In</button>
+
+            </form>
+        </div>
+    )
+}
+export default LoginPage
